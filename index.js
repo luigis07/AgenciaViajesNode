eb73690d83f1adfb278fb960041059ad33fb4084
Luis Gonzalez <luigis07@hotmail.com>
2023-08-30 14:35:20 -0600

    Proyecto Agencia de Viajes

---
+++ index.js
@@ -0,0 +1,22 @@
+//const express = require('express');
+import express from 'express';
+
+const app = express();
+
+// Definir puerto
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => { // req - lo que enviamos : res - lo que express nos responde
+    res.send('Inicio');
+});
+app.get('/nosotros', (req, res) => { // req - lo que enviamos : res - lo que express nos responde
+    res.send('Nosotros');
+});
+app.get('/contacto', (req, res) => { // req - lo que enviamos : res - lo que express nos responde
+    res.send('Contacto');
+});
+
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+})
+
