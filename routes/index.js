0ea8c7dbe9d2b505f4fdd8581fecb81c6bb6532b
Luis Gonzalez <luigis07@hotmail.com>
2023-09-08 21:53:36 -0600

    Agregando bloques

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ import {
     paginaNosotros, 
     paginaViajes, 
     paginaTestimoniales, 
-    paginaDetalleViaje } from '../controllers/paginasController.js';
+    paginaDetalleViaje 
+} from '../controllers/paginasController.js';
+import { guardarTestimonial } from '../controllers/testimonialController.js';
 
 const router = express.Router();
 
@@ -17,6 +19,8 @@ router.get('/viajes', paginaViajes);
 router.get('/viajes/:slug', paginaDetalleViaje);
 
 router.get('/testimoniales', paginaTestimoniales);
+
+router.post('/testimoniales', guardarTestimonial);
 
 // router.get('/contacto', (req, res) => {
 //     res.send('Contacto');
